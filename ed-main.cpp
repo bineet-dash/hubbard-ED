c6afec76ae3e8ace5a94200c4f822696871260f2
Bineet Dash <bineetdash@gmail.com>
2018-01-07 11:30:42 +0530

    added main diagonalization routine.

---
+++ ed-main.cpp
@@ -0,0 +1,58 @@
+#include <fstream>
+#include "ed_library.h"
+#include "common_globals.h"
+
+int main(int argc, char* argv[])
+{
+  cout << "Enter lattice size and U: ";
+  cin >> size >> U; assert(size%2==0);
+
+  vector<basis> half_filling; select_half_filling(half_filling);
+  std::vector<basis> v_spin;
+  std::vector<double> eigenvalues;
+
+  int spin_limit = int(0.5*size);
+
+  for(int i= -spin_limit; i<=spin_limit; i++)
+  {
+    select_spin(half_filling, v_spin, i);
+    cout << "Spin: " << i << " sector\n----------------\nsize=" << v_spin.size() << endl;
+
+    MatrixXd Ht; construct_Ht(Ht,v_spin);
+    MatrixXd HU; construct_HU(HU,v_spin);
+    MatrixXd H=Ht+HU;
+
+    std::vector<double> ith_spin_eivals; MatrixXd ith_eigenvectors;
+    diagonalize(H, ith_spin_eivals, ith_eigenvectors);
+
+    eigenvalues.insert(eigenvalues.end(),ith_spin_eivals.begin(),ith_spin_eivals.end());
+    v_spin.clear(); ith_spin_eivals.clear(); cout << endl;
+  }
+
+  sort(eigenvalues.begin(),eigenvalues.end());
+  filter(eigenvalues);
+
+  ofstream fout; string filename;
+  filename = "data/eivals_size"+to_string(size)+"_U"+to_string(int(U))+".txt";
+  fout.open(filename);
+  for(auto it=eigenvalues.begin(); it!=eigenvalues.end(); it++) fout << *it << endl;
+  fout.close();
+
+  eigenvalues.clear();
+  return 0;
+}
+
+void check_consistency(double t, double U)
+{
+  if(size!=2) return;
+  cout << "-------------------------------------\n";
+  cout << "Theoretical result for eigenvalues: \n";
+  cout << U/2-sqrt(U*U/4+4*t*t) << endl << 0 << " (Triplet) " << endl << U << endl << U/2+sqrt(U*U/4+4*t*t)<< endl;
+}
+
+void check_tb_validity(void)
+{
+  cout << "The eigenvalues obtained by theoretical TB model: \n";
+  for(int i=0; i<size; i++) cout << -2*t*cos(2*M_PI*i/double(size)) << ", ";
+  cout << endl;
+}
