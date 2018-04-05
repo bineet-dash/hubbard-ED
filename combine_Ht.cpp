429f459a0c638fcd4f0f2d596e4732b213254eb6
Bineet Dash <bineetdash@gmail.com>
2018-04-06 00:28:15 +0530

    Simultaneous generation of Ht

---
+++ combine_Ht.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <locale>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <cstring>
+#include <experimental/filesystem>
+#include <Eigen/Dense>
+#include <fstream>
+#include "ed_library.h"
+
+using namespace std;
+using namespace Eigen;
+
+std::string extract_ints(std::ctype_base::mask category, std::string str, std::ctype<char> const& facet)
+{
+    using std::strlen;
+
+    char const *begin = &str.front(),
+               *end   = &str.back();
+
+    auto res = facet.scan_is(category, begin, end);
+
+    begin = &res[0];
+    end   = &res[strlen(res)];
+
+    return std::string(begin, end);
+}
+
+std::string extract_ints(std::string str)
+{
+    return extract_ints(std::ctype_base::digit, str,
+         std::use_facet<std::ctype<char>>(std::locale("")));
+}
+
+std::vector<std::string> explode(std::string const & s, char delim)
+{
+    std::vector<std::string> result;
+    std::istringstream iss(s);
+
+    for (std::string token; std::getline(iss, token, delim); )
+    {
+        result.push_back(std::move(token));
+    }
+
+    return result;
+}
+
+void get_params(string filename, int& size, int& start_row, int& start_col, int& cluster_size)
+{
+  int params [5]; 
+  auto v = explode(filename, '_');
+  for(int i=0; i<v.size(); i++)
+  {
+    string s = v.at(i);
+    stringstream ss(extract_ints(s)); 
+    ss >> params[i];
+  } 
+  size = params[1];
+  start_row = params[2];
+  start_col = params[3];
+  cluster_size = params[4];
+}
+
+namespace fs = std::experimental::filesystem;
+
+int main(int argc, char* argv[])
+{
+  if(argc!=2) {cout << "give matrix size\n"; exit(1);}
+  int m_size = stoi(argv[1]);
+  MatrixXd Ht = MatrixXd::Constant(m_size,m_size,9);
+
+  for (auto & p : fs::directory_iterator("EDdata/"))
+  {
+    std::ostringstream oss;
+    oss << p;
+    std::string path = oss.str();
+    if ( path.front() == '"' ) 
+    {
+      path.erase( 0, 1 ); // erase the first character
+      path.erase( path.size() - 1 ); // erase the last character
+    }
+    cout << path << endl;
+    ifstream fin(path);
+    if(fin.fail())
+    {
+      cout << path << " not avaliable." << endl;
+      continue; 
+    }
+
+    int size, start_row, start_col, csize;
+    get_params(path, size, start_row, start_col, csize);
+
+    for(int i=start_row; i<start_row+csize; i++)
+    {
+      for(int j=start_col; j<start_col+csize; j++)
+       {
+         fin >> Ht(i,j); 
+        //  double a; fin >> a; cout << a; 
+       }
+    }
+    fin.close();
+  }   
+
+  // VectorXd lambda; MatrixXd vc;
+  // diagonalize(Ht, lambda, vc);
+  // ofstream fout("check_eivals.txt");
+  // fout << lambda << endl;
+  // fout.close();
+  
+  ofstream fout;
+  fout.open("check_Ht.txt");
+  fout << Ht << endl;
+  fout.close();
+  
+  return 0;
+}
---
+++ parallel_Ht.cpp
@@ -0,0 +1,59 @@
+#include <fstream>
+#include "ed_library.h"
+#include "common_globals.h"
+
+int main(int argc, char* argv[])
+{
+   // cout << "Enter lattice size and U: ";
+   // cin >> size >> U; assert(size%2==0);
+   int start_row, start_col, cluster_size;
+   if(argc!=6) exit(1);
+   start_row = std::strtol(argv[1],nullptr,0);
+   start_col = std::strtol(argv[2],nullptr,0);
+   cluster_size =  std::strtol(argv[3],nullptr,0);
+   size =  std::strtol(argv[4],nullptr,0);
+   U =  std::strtol(argv[5],nullptr,0);
+
+   vector<basis> half_filling;
+   std::vector<basis> v_spin;
+   char verbose_pref = 'y';
+   int spin = 0;
+
+   select_half_filling(half_filling);
+   select_spin(half_filling, v_spin, spin);
+
+   string filename = "EDdata/ED_size="+to_string(size)+"_row="+to_string(start_row)+"_col="+to_string(start_col)+"_csize="+to_string(cluster_size)+".txt";
+   ofstream fout(filename);
+
+   for(int a = start_row; a < start_row+cluster_size; a++)
+   {
+      for(int b = start_col; b < start_col+cluster_size; b++)
+      {
+         double elem=0.0;     //Ht(a,b)
+         for(int sigma=-1; sigma<=1; sigma+=2)//sum over sigma
+         {
+            for(int i=0; i<size-1; i++)   //c\dagger_i c_i+1
+            {
+               int temp=annhilate(v_spin.at(b).get_x(),i+1,sigma);
+               (v_spin.at(a).get_x()==create(temp,i,sigma))? elem+= -t: elem+=0;
+               (v_spin.at(a).get_x()==-create(temp,i,sigma))? elem+= t: elem+=0;
+            }
+
+            for(int i=0; i<size-1; i++) //c\dagger_i+1 c_i
+            {
+               int temp=annhilate(v_spin.at(b).get_x(),i,sigma);
+               (v_spin.at(a).get_x()==create(temp,i+1,sigma))? elem+= -t: elem+=0;
+               (v_spin.at(a).get_x()==-create(temp,i+1,sigma))? elem+= t: elem+=0;
+            }
+         }
+
+         fout << elem << " ";   
+         if(verbose_pref=='y') cout << a << " " << b << "\r";
+      }
+      fout << endl;
+   }
+
+   fout.close();
+   v_spin.clear();
+   return 0;
+}
